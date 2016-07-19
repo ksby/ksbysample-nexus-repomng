8c409c4b7395945583629a5de90ca59c2db9ac16
ksby <kusa@db3.so-net.ne.jp>
2016-07-20 03:48:46 +0900

    #1 ksbysample-library-simpleutils ライブラリを利用する Spring Boot のサンプルプロジェクトを作成しました

---
+++ ksbysample-webapp-demo/src/main/java/ksbysample/webapp/demo/KsbysampleWebappDemoApplication.java
@@ -0,0 +1,12 @@
+package ksbysample.webapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsbysampleWebappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsbysampleWebappDemoApplication.class, args);
+	}
+}
---
+++ ksbysample-webapp-demo/src/main/java/ksbysample/webapp/demo/SampleController.java
@@ -0,0 +1,20 @@
+package ksbysample.webapp.demo;
+
+import ksbysample.library.simpleutils.StringListUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+
+    @RequestMapping
+    @ResponseBody
+    public String index() {
+        return StringListUtils.convertStringList2LengthCsv(Arrays.asList("a", "ab", "abc"));
+    }
+
+}
