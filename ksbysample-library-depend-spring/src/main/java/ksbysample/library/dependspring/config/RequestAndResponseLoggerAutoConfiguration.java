3bf9d7833584ab2db963ac65207cfefdfc8a4c7d
ksby <kusa@db3.so-net.ne.jp>
2016-12-25 10:50:15 +0900

    #3 RequestAndResponseLogger.java, RequestAndResponseLoggerAutoConfiguration.java を実装しました。

---
+++ ksbysample-library-depend-spring/src/main/java/ksbysample/library/dependspring/config/RequestAndResponseLoggerAutoConfiguration.java
@@ -0,0 +1,34 @@
+package ksbysample.library.dependspring.config;
+
+import ksbysample.library.dependspring.intercepter.RequestAndResponseLogger;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RequestAndResponseLoggerAutoConfiguration {
+
+    private static final String REQUESTMAPPING_EXPRESSION = "@annotation(org.springframework.web.bind.annotation.RequestMapping)";
+
+    @Value("${ksbysample.library.request-and-response-logger.execution:}")
+    private String execution;
+
+    @Bean
+    @ConditionalOnWebApplication
+    public Advisor requestAndResponseLoggerAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        String expression = StringUtils.EMPTY;
+        if (StringUtils.isNotBlank(this.execution)) {
+            expression = this.execution + " && ";
+        }
+        expression += REQUESTMAPPING_EXPRESSION;
+        pointcut.setExpression(expression);
+        return new DefaultPointcutAdvisor(pointcut, new RequestAndResponseLogger());
+    }
+
+}
